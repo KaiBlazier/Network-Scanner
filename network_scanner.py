9a6005f82a2aba69f09c6be479ac875625ee6f33
Kai Blazier <kaiblazier16@gmail.com>
2024-12-19 23:20:22 -0600

    I made a mistake pushing the code. I forgot to save the file before pushing so nothing was added

---
+++ network_scanner.py
@@ -0,0 +1,28 @@
+from scapy.all import ARP, Ether, srp
+
+def scan(ip):
+    arp = ARP(pdst=ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    clients = []
+    for sent, received in result:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'vendor': get_vendor(received.hwsrc)})
+
+    return clients
+
+def get_vendor(mac):
+    # Placeholder function to get vendor information from MAC address
+    # You can use an API or a local database to get the actual vendor information
+    return "Unknown Vendor"
+
+if __name__ == "__main__":
+    ip_range = "192.168.1.1/24"
+    clients = scan(ip_range)
+    print("Available devices in the network:")
+    print("IP" + " "*18+"MAC" + " "*18+"Vendor")
+    for client in clients:
+        print("{:16}    {:18}    {}".format(client['ip'], client['mac'], client['vendor']))
+
